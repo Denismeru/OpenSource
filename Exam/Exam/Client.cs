15fb4ac74d7cb3d16d18eb00c3153396f7d7f921
Denismeru <jiukvyuk@gmail.com>
2024-05-17 21:33:28 +0300

    1.0.0.0

---
+++ Exam/Exam/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientApp;
+
+namespace Exam
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.Pamagiti();
+            ClientApp.Client client = new ClientApp.Client();
+            client.Open();
+
+        }
+    }
+}
